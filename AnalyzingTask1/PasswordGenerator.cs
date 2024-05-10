965d28783109d0c24c251af715a4c4c67f56814f
Rasul Ramazanov <Rasul_Ramazanov@epam.com>
2024-05-10 16:00:53 +0300

    Add project files.

---
+++ AnalyzingTask1/PasswordGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Cryptography;
+
+namespace AnalyzingTask1
+{
+    public class PasswordGenerator
+    {
+        public string GeneratePasswordHashUsingSalt(string passwordText, byte[] salt)
+        {
+            var iterate = 10000;
+            var pbkdf2 = new Rfc2898DeriveBytes(passwordText, salt, iterate);
+            byte[] hash = pbkdf2.GetBytes(20);
+
+            byte[] hashBytes = new byte[36];
+            Array.Copy(salt, 0, hashBytes, 0, 16);
+            Array.Copy(hash, 0, hashBytes, 16, 20);
+
+            var passwordHash = Convert.ToBase64String(hashBytes);
+
+            return passwordHash;
+
+        }
+    }
+}
---
+++ AnalyzingTask1/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AnalyzingTask1;
+using System.Security.Cryptography;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        var generator = new PasswordGenerator();
+
+        string passwordText = "MySuperSecurePassword";
+        byte[] salt;
+        new RNGCryptoServiceProvider().GetBytes(salt = new byte[16]);
+        generator.GeneratePasswordHashUsingSalt(passwordText, salt);
+
+        Console.WriteLine("Hello world");
+    }
+}
