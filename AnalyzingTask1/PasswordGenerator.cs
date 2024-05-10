196161f9adab73e5f376eab5a27882619f8b98f1
Rasul Ramazanov <Rasul_Ramazanov@epam.com>
2024-05-10 16:01:11 +0300

    Task 1

--- AnalyzingTask1/PasswordGenerator.cs
+++ AnalyzingTask1/PasswordGenerator.cs
@@ -7,17 +7,16 @@ namespace AnalyzingTask1
         public string GeneratePasswordHashUsingSalt(string passwordText, byte[] salt)
         {
             var iterate = 10000;
-            var pbkdf2 = new Rfc2898DeriveBytes(passwordText, salt, iterate);
-            byte[] hash = pbkdf2.GetBytes(20);
+            using (var pbkdf2 = new Rfc2898DeriveBytes(passwordText, salt, iterate))
+            {
+                byte[] hash = pbkdf2.GetBytes(20);
 
-            byte[] hashBytes = new byte[36];
-            Array.Copy(salt, 0, hashBytes, 0, 16);
-            Array.Copy(hash, 0, hashBytes, 16, 20);
+                byte[] hashBytes = new byte[36];
+                Buffer.BlockCopy(salt, 0, hashBytes, 0, 16);
+                Buffer.BlockCopy(hash, 0, hashBytes, 16, 20);
 
-            var passwordHash = Convert.ToBase64String(hashBytes);
-
-            return passwordHash;
-
+                return Convert.ToBase64String(hashBytes);
+            }
         }
     }
 }
